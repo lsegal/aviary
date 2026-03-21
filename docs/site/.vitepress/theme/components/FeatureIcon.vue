7cd771744cf344c0ce7446498f69efdeb97064fe
Loren Segal <lsegal@soen.ca>
2026-03-20 19:38:05 -0700

    feat: add FeatureIcon and MessagingLogo components for enhanced UI icons

---
+++ docs/site/.vitepress/theme/components/FeatureIcon.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {
+	AdjustmentsHorizontalIcon,
+	ChartBarSquareIcon,
+	ChatBubbleLeftRightIcon,
+	CommandLineIcon,
+	ClockIcon,
+	WrenchScrewdriverIcon,
+} from "@heroicons/vue/24/outline";
+import { computed } from "vue";
+
+const props = defineProps<{
+	name: "chat" | "clock" | "chart" | "tools" | "controls" | "cli";
+}>();
+
+const icon = computed(() => {
+	switch (props.name) {
+		case "chat":
+			return ChatBubbleLeftRightIcon;
+		case "clock":
+			return ClockIcon;
+		case "chart":
+			return ChartBarSquareIcon;
+		case "tools":
+			return WrenchScrewdriverIcon;
+		case "controls":
+			return AdjustmentsHorizontalIcon;
+		case "cli":
+			return CommandLineIcon;
+	}
+});
+</script>
+
+<template>
+	<div class="panel-icon" aria-hidden="true">
+		<component :is="icon" />
+	</div>
+</template>
