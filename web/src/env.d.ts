444733afde2e284ece9e08edc46750ede6f92d28
Loren Segal <lsegal@soen.ca>
2026-06-25 21:51:20 -0700

    bug: make css import declaration ambient

--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -1,18 +1,14 @@
 // Environment types for import.meta.env used in the web app
 // This file ensures TypeScript understands import.meta.env.DEV and other fields.
 
-declare global {
-	interface ImportMetaEnv {
-		readonly DEV?: boolean;
-		readonly PROD?: boolean;
-		readonly [key: string]: unknown;
-	}
+interface ImportMetaEnv {
+	readonly DEV?: boolean;
+	readonly PROD?: boolean;
+	readonly [key: string]: unknown;
+}
 
-	interface ImportMeta {
-		readonly env: ImportMetaEnv;
-	}
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
 }
 
 declare module "*.css";
-
-export {};
